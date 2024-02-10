bf8edc9021a1023c0581b78c91665a67ebcee6b3
Tom Casavant <tfcasavant@gmail.com>
2024-02-10 10:31:42 -0500

    updated bot to generate polls and take actions

---
+++ bot.py
@@ -0,0 +1,155 @@
+from gb import Gameboy
+import random
+import time
+from mastodon import Mastodon
+import toml
+
+class Bot:
+
+    def __init__(self, config_path="config.toml"):
+        with open('config.toml', 'r') as config_file:
+            self.config = toml.load(config_file)
+            self.mastodon_config = self.config.get('mastodon', {})
+            self.gameboy_config = self.config.get('gameboy', {})
+
+        self.mastodon = self.login()
+        print(self.gameboy_config.get('rom'))
+        self.gameboy = Gameboy(self.gameboy_config.get('rom'), True)
+
+    def simulate(self):
+        while True:
+            #print(self.gameboy.is_running())
+            if True:
+                #self.gameboy.random_button()
+                buttons = {
+                 "a" : self.gameboy.a,
+                 "b" : self.gameboy.b,
+                 "start" : self.gameboy.start,
+                 "select" : self.gameboy.select,
+                 "up" : self.gameboy.dpad_up,
+                 "down" : self.gameboy.dpad_down,
+                 "right" : self.gameboy.dpad_right,
+                 "left" : self.gameboy.dpad_left,
+                 "random" : self.gameboy.random_button,
+                 "tick" : "tick"
+                }
+                #self.gameboy.random_button()
+                print(buttons)
+                press = input("Button: ")
+                if press == "tick":
+                    for i in range(60):
+                        self.gameboy.pyboy.tick()
+                else:
+                    buttons[press]()
+                    self.gameboy.pyboy.tick()
+                #time.sleep(1)
+
+    def login(self):
+        server = self.mastodon_config.get('server')
+        print(f"Logging into {server}")
+        return Mastodon(access_token=self.mastodon_config.get('access_token'), api_base_url=server)
+
+    def post_poll(self, status, options, expires_in=60*60, reply_id=None):
+        poll = self.mastodon.make_poll(options, expires_in=expires_in, hide_totals=False)
+        return self.mastodon.status_post(status, in_reply_to_id=reply_id, language='en', poll=poll)
+
+    def save_ids(self, post_id, poll_id):
+        with open('ids.txt', 'w') as file:
+            file.write(f"{post_id},{poll_id}")
+
+    def read_ids(self):
+        try:
+            with open('ids.txt', 'r') as file:
+                content = file.read()
+                if content:
+                    post_id, poll_id = content.split(',')
+                    return post_id, poll_id
+        except FileNotFoundError:
+            return None, None
+
+    def pin_posts(self, post_id, poll_id):
+        self.mastodon.status_pin(poll_id)
+        self.mastodon.status_pin(post_id)
+
+    def unpin_posts(self, post_id, poll_id):
+        self.mastodon.status_unpin(post_id)
+        self.mastodon.status_unpin(poll_id)
+
+    def take_action(self, result):
+        buttons = {
+            "up ⬆️": self.gameboy.dpad_up,
+            "down ⬇️": self.gameboy.dpad_down,
+            "right ➡️": self.gameboy.dpad_right,
+            "left ⬅️": self.gameboy.dpad_left,
+            "🅰": self.gameboy.a,
+            "🅱": self.gameboy.b,
+            "start": self.gameboy.start,
+            "select": self.gameboy.select
+        }
+
+        # Perform the corresponding action
+        if result.lower() in buttons:
+            action = buttons[result.lower()]
+            action()
+        else:
+            print(f"No action defined for '{top_result_title}'.")
+
+    def run(self):
+        self.gameboy.load()
+        post_id, poll_id = self.read_ids()
+        top_result = None
+
+        if post_id:
+            try:
+                self.unpin_posts(post_id, poll_id)
+            except:
+                time.sleep(30)
+                self.unpin_posts(post_id, poll_id)
+
+            poll_status = self.mastodon.status(poll_id)
+            poll_results = poll_status.poll['options']
+            max_result = max(poll_results, key=lambda x: x['votes_count'])
+            if (max_result['votes_count'] == 0):
+                self.gameboy.random_button()
+            self.take_action(top_result)
+
+        self.gameboy.tick(300) # Progress the screen to the next position
+        image = self.gameboy.screenshot()
+        try:
+            media = self.mastodon.media_post(image, description='Image of pokemon rom')
+        except:
+            time.sleep(45)
+            media = self.mastodon.media_post(image, description='Image of pokemon rom')
+
+        time.sleep(50)
+        try:
+            post = self.mastodon.status_post(f"(Test) The highest voted result was {top_result}", media_ids=[media['id']])
+        except:
+            time.sleep(30)
+            post = self.mastodon.status_post(f"(Test) The highest voted result was {top_result}", media_ids=[media['id']])
+
+        try:
+            poll = self.post_poll("Pick one of the options:", ["Up ⬆️", "Down ⬇️", "Right ➡️ ", "Left ⬅️", "🅰", "🅱", "Start", "Select"], reply_id=post['id'])
+        except:
+            time.sleep(30)
+            poll = self.post_poll("Pick one of the options:", ["Up ⬆️", "Down ⬇️", "Right ➡️ ", "Left ⬅️", "🅰", "🅱", "Start", "Select"], reply_id=post['id'])
+
+        try:
+            self.pin_posts(post['id'], poll['id'])
+        except:
+            time.sleep(30)
+            self.pin_posts(post['id'], poll['id'])
+
+        self.save_ids(post['id'], poll['id'])
+
+        # Save game state
+        self.gameboy.save()
+
+if __name__ == '__main__':
+    bot = Bot()
+    bot.run()
+    # for i in range(2):
+    #    bot.run()
+    #    time.sleep(60)
+    #bot.simulate()
+
--- gb.py
+++ gb.py
@@ -1,38 +1,113 @@
 from pyboy import PyBoy, WindowEvent
-
+import random
+import threading
+import os
 class Gameboy:
-
 
     def __init__(self, rom, debug=False):
         self.debug = debug
-        self.pyboy = self.load_rom(rom)
+        self.rom = rom
+        self.running = False
+        self.pyboy = self.load_rom(self.rom)
+        self.pyboy.set_emulation_speed(0)
+
+    def start_thread(self):
+        self.pyboy = self.load_rom(self.rom)
+        self.pyboy.set_emulation_speed(0)
+        print(self.pyboy)
+        #self.run()
+
+    def is_running(self):
+        return self.running
+
+    def run(self) -> None:
+        self.running = True
+        while True:
+            self.random_button()
+
+    def tick(self, ticks=1):
+        for tick in range(ticks):
+            self.pyboy.tick()
+
+    def stop(self):
+        self.running = False
 
     def load_rom(self, rom):
-        return PyBoy(rom, window_type="SDL2" if self.debug else "headless", window_scale=3, debug=self.debug, game_wrapper=True)
+        return PyBoy(rom, window_type="SDL2" if self.debug else "headless", window_scale=3, debug=False, game_wrapper=True)
 
     def dpad_up(self) -> None:
-        pyboy.send_input(WindowEvent.PRESS_ARROW_UP)
+        self.pyboy.send_input(WindowEvent.PRESS_ARROW_UP)
+        self.tick()
+        self.pyboy.send_input(WindowEvent.RELEASE_ARROW_UP)
+        #print(self.pyboy.stop())
+        #self.tick()
 
     def dpad_down(self) -> None:
-        pyboy.send_input(WindowEvent.PRESS_ARROW_DOWN)
+        self.pyboy.send_input(WindowEvent.PRESS_ARROW_DOWN)
+        print("down")
+        self.tick()
+        self.pyboy.send_input(WindowEvent.RELEASE_ARROW_DOWN)
+        #self.tick()
 
     def dpad_right(self) -> None:
-        pyboy.send_input(WindowEvent.PRESS_ARROW_RIGHT)
+        self.pyboy.send_input(WindowEvent.PRESS_ARROW_RIGHT)
+        print("right")
+        self.tick()
+        self.pyboy.send_input(WindowEvent.RELEASE_ARROW_RIGHT)
+        #self.tick()
 
     def dpad_left(self) -> None:
-        pyboy.send_input(WindowEvent.PRESS_ARROW_LEFT)
+        self.pyboy.send_input(WindowEvent.PRESS_ARROW_LEFT)
+        print("left")
+        self.tick()
+        self.pyboy.send_input(WindowEvent.RELEASE_ARROW_LEFT)
+        #self.tick()
 
     def a(self) -> None:
-        pyboy.send_input(WindowEvent.PRESS_BUTTON_A)
+        self.pyboy.send_input(WindowEvent.PRESS_BUTTON_A)
+        print("a")
+        self.tick()
+        self.pyboy.send_input(WindowEvent.RELEASE_BUTTON_A)
+        #self.tick()
 
     def b(self) -> None:
-        pyboy.send_input(WindowEvent.PRESS_BUTTON_B)
+        self.pyboy.send_input(WindowEvent.PRESS_BUTTON_B)
+        print("b")
+        self.tick()
+        self.pyboy.send_input(WindowEvent.RELEASE_BUTTON_B)
+        #self.tick()
 
     def start(self) -> None:
-        pyboy.send_input(WindowEvent.PRESS_BUTTON_START)
+        self.pyboy.send_input(WindowEvent.PRESS_BUTTON_START)
+        print("start")
+        self.tick()
+        self.pyboy.send_input(WindowEvent.RELEASE_BUTTON_START)
+        #self.tick()
 
     def select(self) -> None:
-        pyboy.send_input(WindowEvent.PRESS_BUTTON_SELECT)
+        self.pyboy.send_input(WindowEvent.PRESS_BUTTON_SELECT)
+        print("select")
+        self.tick()
+        self.pyboy.send_input(WindowEvent.RELEASE_BUTTON_SELECT)
 
     def screenshot(self):
-        return pyboy.screen_image()
+        self.pyboy.screen_image().save('screenshot.png')
+        return 'screenshot.png'
+
+    def random_button(self):
+        button = random.choice([self.dpad_up, self.dpad_down, self.dpad_right, self.dpad_left, self.a, self.b, self.start, self.select])
+        button()
+
+    def load(self):
+        if os.path.exists("save.state"):
+            with open("save.state", "rb") as file:
+                self.pyboy.load_state(file)
+            return True
+        else:
+            print("Save state does not exist")
+            return False
+
+    def save(self):
+        with open("save.state", "wb") as file:
+            self.pyboy.save_state(file)
+
